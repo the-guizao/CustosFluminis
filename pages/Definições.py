d162cc1ca637cf84ee2b1ec945c898fba5fa5022
Guilherme Netto <121568584+the-guizao@users.noreply.github.com>
2025-10-07 08:32:50 -0300

    Add files via upload

---
+++ pages/Definições.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title("Definições importantes!")
+st.markdown("Observe que durante o trabalho, utilizamos alguns termos técnicos. Vamos nos atentar ao que cada um significa:")
+#XGBoost - Texto explicativo
+st.header("XGBoost")
+st.subheader("O método poligonal")
+st.write("O método XGBoost serve como...")
+st.latex("\hat{f}_{(0)}(x)=argmin\sum_{i=1}^{N}L(y_i,theta)")
+#LSTM
+st.header("LSTM")
+st.subheader("Redes Neurais à serviço da população")
+st.write("Uma rede neural é...")
---
+++ pages/O Projeto.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("O projeto")
+st.header("Problemática")
+st.subheader("O Desasatre do Rio Doce")
+st.write("Em 2015, em Mariana-MG, ocorreu o rompimento...")
